85bd3894b7b07aaf5ce1884243d1a0f787e9cff9
nmanningbmj <102024038+nmanningbmj@users.noreply.github.com>
2023-01-16 11:12:29 +0000

    password visibility

--- index.js
+++ index.js
@@ -1,36 +1,50 @@
-const passwordInput = document.querySelector(".visibility-icon");
-const input = document.querySelector("#pwd");
+const icon = document.querySelector(".visibility-icon");
 const img = document.querySelector("img");
+
+const inputPass = document.querySelector("#pwd");
 let labels = document.querySelector("label");
-
+const formFields = document.querySelectorAll("input");
 const email = document.querySelector("#email");
-const pwd = document.querySelector("#pwd");
 
-img.addEventListener("click", () => {
-  if (input.type === "password") {
-    img.src = "images/visibility_on.svg";
-    img.alt = "show password";
-    input.type = "text";
-  } else {
-    img.src = "images/visibility_off.svg";
-    img.alt = "hide password";
-    input.type = "password";
-  }
-});
+const confirm = document.getElementById("confirm");
+const secondPass = document.getElementById("SecondPwd");
 
-//floating labels on input
-[email, pwd].forEach(function (e) {
-  e.addEventListener("focus", function () {
+formFields.forEach((i) => {
+  i.addEventListener("focus", function () {
     this.previousElementSibling.classList.add("focus");
   });
 });
 
-[email, pwd].forEach(function (e) {
-  e.addEventListener("keyup", function () {
+formFields.forEach((i) => {
+  i.addEventListener("keyup", function () {
     if (this.value.length >= 1) {
       this.previousElementSibling.classList.add("focus");
     } else {
       this.previousElementSibling.classList.remove("focus");
     }
   });
+});
+
+icon.addEventListener("click", function () {
+  if (inputPass.type === "password") {
+    img.src = "images/visibility_on.svg";
+    img.alt = "show password";
+    inputPass.type = "text";
+  } else {
+    img.src = "images/visibility_off.svg";
+    img.alt = "hide password";
+    inputPass.type = "password";
+  }
+});
+
+confirm.addEventListener("click", function () {
+  if (secondPass.type === "password") {
+    confirm.src = "images/visibility_on.svg";
+    confirm.alt = "show password";
+    secondPass.type = "text";
+  } else {
+    confirm.src = "images/visibility_off.svg";
+    confirm.alt = "hide password";
+    secondPass.type = "password";
+  }
 });

1b84850d00b07860be5c9b288767045f0c8cd2bb
nmanningbmj <102024038+nmanningbmj@users.noreply.github.com>
2023-01-17 11:58:23 +0000

    form state styling

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const icon = document.querySelector(".visibility-icon");
-const img = document.querySelector("img");
+const img = document.querySelector(".visibility-icon");
 
 const inputPass = document.querySelector("#password");
 let labels = document.querySelector("label");
@@ -40,7 +40,7 @@ const checkEmail = () => {
   } else if (!isEmailValid(email)) {
     showError(emailEl, "Email is not valid.");
   } else {
-    showError(emailEl, "");
+    showSuccess(emailEl, "");
     valid = true;
   }
   return valid;
@@ -54,7 +54,7 @@ const checkPassword = () => {
   if (!isRequired(password)) {
     showError(passwordEl, "Password cannot be blank.");
   } else {
-    showError(passwordEl, "");
+    showSuccess(passwordEl, "");
     valid = true;
   }
   return valid;
@@ -71,14 +71,18 @@ const checkConfirmPassword = () => {
   } else if (!isPasswordSecure(password)) {
     showError(
       passwordEl,
-      "Password must has at least 8 characters that include at least 1 lowercase character, 1 uppercase characters, 1 number, and 1 special character in (!@#$%^&*)"
+      "Password must has at least 8 characters that includes one number"
+    );
+    showError(
+      confirmPasswordEl,
+      "Password must has at least 8 characters that includes one number"
     );
   } else if (password !== confirmedPassword) {
     showError(passwordEl, "passwords do not match");
     showError(confirmPasswordEl, "passwords do not match");
   } else {
-    showError(passwordEl, "");
-    showError(confirmPasswordEl, "");
+    showSuccess(passwordEl, "");
+    showSuccess(confirmPasswordEl, "");
 
     valid = true;
   }
@@ -86,9 +90,9 @@ const checkConfirmPassword = () => {
 };
 
 const isEmailValid = (email) => {
-  const re =
+  const reg =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
+  return reg.test(email);
 };
 
 const isPasswordSecure = (password) => {
@@ -98,30 +102,39 @@ const isPasswordSecure = (password) => {
 
 const isRequired = (value) => (value === "" ? false : true);
 
+const errorIcon = document.getElementById("#error-icon");
+
 const showError = (input, message) => {
-  // get the form-field element
   const formField = input.parentElement;
-  // add the error class
   formField.classList.add("error");
 
-  // show the error message
   const error = formField.querySelector("small");
   error.textContent = message;
+
+  if (formField.classList.contains("error")) {
+    error.classList.add("error-icon");
+  }
+};
+
+const showSuccess = (input) => {
+  // get the form-field element
+  const formField = input.parentElement;
+
+  // remove the error class
+  formField.classList.remove("error");
+
+  // hide the error message
+  const error = formField.querySelector("small");
+  error.textContent = "";
 };
 
 //first step login page
 if (loginForm) {
   loginForm.addEventListener("submit", function (e) {
-    // prevent the form from submitting
     e.preventDefault();
-
-    // validate fields
     let isEmailValid = checkEmail(),
       isPasswordValid = checkPassword();
-
     let isFormValid = isEmailValid && isPasswordValid;
-
-    // submit to the server if the form is valid
     if (isFormValid) {
       console.log("valid");
     }
@@ -131,32 +144,21 @@ if (loginForm) {
 //second step login page
 if (sendEmail) {
   sendEmail.addEventListener("submit", function (e) {
-    // prevent the form from submitting
     e.preventDefault();
-
-    // validate fields
     let isValid = checkEmail();
-
     let isFormValid = isValid;
-
-    // submit to the server if the form is valid
     if (isFormValid) {
       console.log("valid");
     }
   });
 }
 
+//final stage of reset password journey
 if (updatePassword) {
   updatePassword.addEventListener("submit", function (e) {
-    // prevent the form from submitting
     e.preventDefault();
-
-    // validate fields
     let isEmailValid = checkConfirmPassword();
-
     let isFormValid = isEmailValid;
-
-    // submit to the server if the form is valid
     if (isFormValid) {
       console.log("valid");
     }
